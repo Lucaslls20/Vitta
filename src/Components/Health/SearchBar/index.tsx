d84bd4bc25339aee23d4c892c5eb5d7ac4205a8e
Lucas <lucano20bhz@gmail.com>
2025-03-08 11:57:21 -0300

    Mudanças na SearchBar da tela Health

--- src/Components/Health/SearchBar/index.tsx
+++ src/Components/Health/SearchBar/index.tsx
@@ -11,10 +11,10 @@ interface SearchBarProps {
 export const SearchBar = ({ searchQuery, onSearchChange }: SearchBarProps) => (
   <TextInput
     placeholder="Search"
-    placeholderTextColor={COLORS.shadow}
+    placeholderTextColor={COLORS.primary}
     style={styles.searchInput}
-    theme={{ colors: { primary: COLORS.shadow } }}
-    right={<TextInput.Icon icon="magnify" color={COLORS.shadow} />}
+    theme={{ colors: { primary: COLORS.primary } }}
+    right={<TextInput.Icon icon="magnify" color={COLORS.primary} />}
     accessibilityLabel="Search field"
     value={searchQuery}
     onChangeText={onSearchChange}
