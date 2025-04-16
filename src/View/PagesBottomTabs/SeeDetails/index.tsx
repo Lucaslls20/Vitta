0731644a15b9c5f9c07cb66b2cb304d29b9acef3
Lucas <lucano20bhz@gmail.com>
2025-04-16 20:30:02 -0300

    Mudança do Dialog pelo Toast na tela SeeDetails.

--- src/View/PagesBottomTabs/SeeDetails/index.tsx
+++ src/View/PagesBottomTabs/SeeDetails/index.tsx
@@ -3,7 +3,8 @@ import { ScrollView, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useRecipeDetailsViewModel } from '../../../ViewModels/RecipeDetailsViewModel';
 import { SeeDetailsRouteProp } from '../../../App';
-import { Title, Provider, Portal, Dialog, Button, Paragraph, Text } from 'react-native-paper';
+import { Title, Provider, Button, Text } from 'react-native-paper';
+import Toast from 'react-native-toast-message';
 import { styles } from './styles';
 import { HeaderComponent } from '../../../Components/SeeDetails/HeaderComponent';
 import { RecipeImage } from '../../../Components/SeeDetails/RecipeImage';
@@ -14,29 +15,27 @@ import { RecipeInfo } from '../../../Components/SeeDetails/RecipeInfo';
 import { Ingredients } from '../../../Components/SeeDetails/Ingredients';
 import { PreparationSteps } from '../../../Components/SeeDetails/PreparationSteps';
 import { FavoriteButton } from '../../../Components/SeeDetails/FavoriteButton';
-import { COLORS } from '../../Colors';
 
 const SeeDetails = ({ route }: { route: SeeDetailsRouteProp }) => {
   const { recipeId } = route.params;
   const { recipeDetails, loading, error, saveFavoriteRecipe, saveError, saveSuccess, isFavorite } = useRecipeDetailsViewModel(recipeId);
   const navigation = useNavigation();
 
-  // Estados para controlar o diálogo
-  const [dialogVisible, setDialogVisible] = useState(false);
-  const [dialogTitle, setDialogTitle] = useState('');
-  const [dialogMessage, setDialogMessage] = useState('');
-
   // Atualiza os estados conforme os erros ou sucesso de salvar
   useEffect(() => {
     if (saveError) {
-      setDialogTitle('Erro');
-      setDialogMessage(saveError);
-      setDialogVisible(true);
+      Toast.show({
+        type: 'error',
+        text1: 'Erro',
+        text2: saveError,
+      });
     }
     if (saveSuccess) {
-      setDialogTitle('Sucesso');
-      setDialogMessage('Receita salva nos favoritos!');
-      setDialogVisible(true);
+      Toast.show({
+        type: 'success',
+        text1: 'Success',
+        text2: 'Recipe saved to favorites!',
+      });
     }
   }, [saveError, saveSuccess]);
 
@@ -73,19 +72,7 @@ const SeeDetails = ({ route }: { route: SeeDetailsRouteProp }) => {
           </ScrollView>
         )}
 
-        {/* Diálogo do react-native-paper */}
-        <Portal>
-          <Dialog visible={dialogVisible} onDismiss={() => setDialogVisible(false)} 
-          style={{backgroundColor:COLORS.white}}>
-            <Dialog.Title style={{color:COLORS.textPrimary}}>{dialogTitle}</Dialog.Title>
-            <Dialog.Content>
-              <Paragraph style={{color:COLORS.textSecondary}}>{dialogMessage}</Paragraph>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button onPress={() => setDialogVisible(false)}><Text style={{color:COLORS.primary}}>Ok</Text></Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
+        <Toast />
       </SafeAreaView>
     </Provider>
   );
