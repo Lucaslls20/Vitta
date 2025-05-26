4dcc5219c899004b9e001b92f3b8a65982fc7789
Lucas <lucano20bhz@gmail.com>
2025-05-26 20:25:43 -0300

    Correção no componente Header.

--- src/Components/Health/Header/index.tsx
+++ src/Components/Health/Header/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Appbar } from 'react-native-paper';
-import { Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { auth, db } from '../../../Services/firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
-import { COLORS } from '../../../View/Colors';
+import { styles } from '../../../View/PagesBottomTabs/Health/styles';
 
 export const Header: React.FC = () => {
   const [userName, setUserName] = useState<string>('Carregando...');
@@ -44,24 +44,9 @@ export const Header: React.FC = () => {
       <TouchableOpacity onPress={() => {}} accessibilityLabel="User profile">
         <Image
          source={{uri:'https://sdmntprsouthcentralus.oaiusercontent.com/files/00000000-2124-61f7-bd61-94477d3bb2a6/raw?se=2025-05-23T23%3A16%3A03Z&sp=r&sv=2024-08-04&sr=b&scid=86969acb-b081-504f-86db-ef41501b4cb1&skoid=732f244e-db13-47c3-bcc7-7ee02a9397bc&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-23T10%3A04%3A33Z&ske=2025-05-24T10%3A04%3A33Z&sks=b&skv=2024-08-04&sig=wgRnMY%2B6B9cJkS6KXajBdP/l/p%2Bq0zAUaE4HaM3Kqe8%3D'}}
-          style={[styles.userAvatar, { width: 50, height: 50, borderRadius: 25 }]}
+          style={[styles.userAvatar, { width: 44, height: 44, borderRadius: 22 }]}
         />
       </TouchableOpacity>
     </Appbar.Header>
   );
 };
-
-const styles = StyleSheet.create({
-  appBar: {
-    backgroundColor: COLORS.secondary,
-    elevation: 4,
-  },
-  appBarTitle: {
-    color: COLORS.textPrimary,
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  userAvatar: {
-    backgroundColor: COLORS.primary, // cor de fundo enquanto carrega
-  },
-});
